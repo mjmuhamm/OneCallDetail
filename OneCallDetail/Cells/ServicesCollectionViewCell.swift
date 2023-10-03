5f53a0339aaacea59cb794877b698ecbec3a4800
Malik Muhammad <mjmuhamm93@gmail.com>
2023-10-03 12:20:36 -0400

    first commit

---
+++ OneCallDetail/Cells/ServicesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesCollectionViewCell.swift
+//  OneCallDetail
+//
+//  Created by Malik Muhammad on 10/1/23.
+//
+
+import UIKit
+
+class ServicesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ OneCallDetail/Controllers/Tabs/ServicesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ServicesViewController.swift
+//  OneCallDetail
+//
+//  Created by Malik Muhammad on 10/1/23.
+//
+
+import UIKit
+
+class ServicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
